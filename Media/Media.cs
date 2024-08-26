cab7ee24090282cb64b9fd6b2518d0fbbebf9f1d
danilospinola <102962780+danilospinola@users.noreply.github.com>
2024-08-26 19:27:43 -0300

    media em c#
    Programa que realiza media em C#

---
+++ Media/Media.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num1;
+        double num2;
+        float media;
+
+        Console.WriteLine("Digite um numero");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("Digite o segundo numero");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        media = (float)((num1 + num2) / 2);
+
+        Console.WriteLine("Sua média é " + media);
+    }
+}
+
