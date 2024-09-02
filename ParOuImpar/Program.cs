63ae3898b780d01ecc2a1925a8e567e1ed0dcef9
danilospinola <102962780+danilospinola@users.noreply.github.com>
2024-09-02 20:38:30 -0300

    Adicionado Projetos
    Adicionado Projetos de ParOuImpar e VerficadorDeIntervalo

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um numero");
+            int numero  = Convert.ToInt32(Console.ReadLine());
+            ParOuImpar(numero);
+        }
+
+        static void ParOuImpar(int Num)
+        {
+            if (Num % 2 == 0 && Num != 0) {
+                Console.WriteLine($"{Num} é Par");
+                Console.ReadKey(); 
+            } else if (Num % 2 != 0)
+            {
+                Console.WriteLine($"{Num} é Impar");
+                Console.ReadKey();
+            }
+            else if (Num == 0) 
+            {
+                Console.WriteLine($"{Num} é Nem par nem impar!");
+                Console.ReadKey();
+            }
+            else { 
+                Console.WriteLine("Erro");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ VerificarIntervalo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarIntervalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Sem operador logico como pedido pelo professor
+            Console.WriteLine("Verificador do intervalo de 10 - 20, digite seu numero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            VerificarIntervalo(numero);
+        }
+
+        static void VerificarIntervalo(int Num)
+        {
+            if(Num >= 10) {
+                if(Num <= 20)
+                {
+                    Console.WriteLine($"{Num} Está dentro do intervalo 10-20");
+                    Console.Read();
+                }
+                else
+                {
+                    Console.WriteLine($"O numero {Num} é maior que 20");
+                    Console.Read();
+                }
+            }
+            Console.WriteLine($"{Num} é menor que 10");
+            Console.Read();
+        }
+    }
+}
